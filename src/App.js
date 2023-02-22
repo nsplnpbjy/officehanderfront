e5600d09b51a683b56704c037b56804bb143f1de
white <nsplnp@126.com>
2023-02-23 00:24:13 +0800

    小修改

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+                <form action="http://127.0.0.1:8095/upload" method="post" enctype="multipart/form-data">
+                    <div>
+                      <label>主表</label>
+                      <input type="file" name='file'/>
+                    </div>
+                    <div>
+                      <label>对比表</label>
+                      <input type="file" name='file'/>
+                    </div>
+                    <div>
+                      <label>相同数据列</label>
+                      <input name="clm"></input>
+                    </div>
+                    <input type="submit" value="上传"/>
+                </form>
+            </div>
       </header>
     </div>
   );
